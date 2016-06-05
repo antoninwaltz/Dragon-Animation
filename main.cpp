2e7f83780916169076a162c7a6fe2dcc7760d47a
Skia <skia@libskia.so>
2016-06-05 16:09:13 +0200

    Display nicely any model

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 #include <vectors.h>
 
 const struct aiScene* scene;
+aiVector3D scene_min, scene_max, scene_center;
 
 Model m;
 Camera c;
@@ -26,75 +27,127 @@ float angle = 0;
 
 /* Initialize OpenGL */
 void initGL() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);               // Set background color to black and opaque
-   glClearDepth(1.0f);                                 // Set background depth to farthest
-   glEnable(GL_CULL_FACE);
-   glEnable(GL_DEPTH_TEST);                            // Enable depth testing
-   glDepthFunc(GL_LEQUAL);                             // Set the type of depth-test
-   glShadeModel(GL_SMOOTH);                            // Enable smooth shading
-   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);               // Set background color to black and opaque
+    glClearDepth(1.0f);                                 // Set background depth to farthest
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);                                // Uses default lighting parameters
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);                            // Enable depth testing
+	glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
+	glEnable(GL_NORMALIZE);
+    glDepthFunc(GL_LEQUAL);                             // Set the type of depth-test
+    glShadeModel(GL_SMOOTH);                            // Enable smooth shading
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
 
 void recursive_render (const struct aiScene *sc, const struct aiNode* nd)
 {
-	unsigned int i;
-	unsigned int n = 0, t;
-	aiMatrix4x4 m = nd->mTransformation;
+    unsigned int i;
+    unsigned int n = 0, t;
+    aiMatrix4x4 m = nd->mTransformation;
 
-	// update transform
-	aiTransposeMatrix4(&m);
-	glPushMatrix();
-	glMultMatrixf((float*)&m);
+    // update transform
+    aiTransposeMatrix4(&m);
+    glPushMatrix();
+    glMultMatrixf((float*)&m);
 
-	// draw all meshes assigned to this node
-	for (; n < nd->mNumMeshes; ++n) {
-		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+    // draw all meshes assigned to this node
+    for (; n < nd->mNumMeshes; ++n) {
+        const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
 
-		// apply_material(sc->mMaterials[mesh->mMaterialIndex]);
+        // apply_material(sc->mMaterials[mesh->mMaterialIndex]);
 
-		if(mesh->mNormals == NULL) {
-			glDisable(GL_LIGHTING);
-		} else {
-			glEnable(GL_LIGHTING);
-		}
+        if(mesh->mNormals == NULL) {
+            glDisable(GL_LIGHTING);
+        } else {
+            glEnable(GL_LIGHTING);
+        }
 
-		for (t = 0; t < mesh->mNumFaces; ++t) {
-			const struct aiFace* face = &mesh->mFaces[t];
-			GLenum face_mode;
+        for (t = 0; t < mesh->mNumFaces; ++t) {
+            const struct aiFace* face = &mesh->mFaces[t];
+            GLenum face_mode;
 
-			switch(face->mNumIndices) {
-				case 1: face_mode = GL_POINTS; break;
-				case 2: face_mode = GL_LINES; break;
-				case 3: face_mode = GL_TRIANGLES; break;
-				default: face_mode = GL_POLYGON; break;
-			}
+            switch(face->mNumIndices) {
+                case 1: face_mode = GL_POINTS; break;
+                case 2: face_mode = GL_LINES; break;
+                case 3: face_mode = GL_TRIANGLES; break;
+                default: face_mode = GL_POLYGON; break;
+            }
 
-			glBegin(face_mode);
+            glBegin(face_mode);
 
-			for(i = 0; i < face->mNumIndices; i++) {
-				int index = face->mIndices[i];
-				if(mesh->mColors[0] != NULL)
-					glColor4fv((GLfloat*)&mesh->mColors[0][index]);
-				if(mesh->mNormals != NULL)
-					glNormal3fv(&mesh->mNormals[index].x);
-				glVertex3fv(&mesh->mVertices[index].x);
-			}
+            for(i = 0; i < face->mNumIndices; i++) {
+                int index = face->mIndices[i];
+                if(mesh->mColors[0] != NULL)
+                    glColor4fv((GLfloat*)&mesh->mColors[0][index]);
+                if(mesh->mNormals != NULL)
+                    glNormal3fv(&mesh->mNormals[index].x);
+                glVertex3fv(&mesh->mVertices[index].x);
+            }
 
-			glEnd();
-		}
+            glEnd();
+        }
 
-	}
+    }
 
-	// draw all children
-	for (n = 0; n < nd->mNumChildren; ++n) {
-		recursive_render(sc, nd->mChildren[n]);
-	}
+    // draw all children
+    for (n = 0; n < nd->mNumChildren; ++n) {
+        recursive_render(sc, nd->mChildren[n]);
+    }
 
-	glPopMatrix();
+    glPopMatrix();
+}
+
+#define min(x,y) (x<y?x:y)
+#define max(x,y) (y>x?y:x)
+void get_bounding_box_for_node (const aiNode* nd,
+        aiVector3D* min,
+        aiVector3D* max,
+        aiMatrix4x4* trafo
+        ){
+    aiMatrix4x4 prev;
+    unsigned int n = 0, t;
+
+    prev = *trafo;
+    aiMultiplyMatrix4(trafo,&nd->mTransformation);
+
+    for (; n < nd->mNumMeshes; ++n) {
+        const aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+        for (t = 0; t < mesh->mNumVertices; ++t) {
+
+            aiVector3D tmp = mesh->mVertices[t];
+            aiTransformVecByMatrix4(&tmp,trafo);
+
+            min->x = min(min->x,tmp.x);
+            min->y = min(min->y,tmp.y);
+            min->z = min(min->z,tmp.z);
+
+            max->x = max(max->x,tmp.x);
+            max->y = max(max->y,tmp.y);
+            max->z = max(max->z,tmp.z);
+        }
+    }
+
+    for (n = 0; n < nd->mNumChildren; ++n) {
+        get_bounding_box_for_node(nd->mChildren[n],min,max,trafo);
+    }
+    *trafo = prev;
+}
+
+
+void get_bounding_box (aiVector3D* min, aiVector3D* max)
+{
+    aiMatrix4x4 trafo;
+    aiIdentityMatrix4(&trafo);
+
+    min->x = min->y = min->z =  1e10f;
+    max->x = max->y = max->z = -1e10f;
+    get_bounding_box_for_node(scene->mRootNode,min,max,&trafo);
 }
 
 void display()
 {
+    float scale;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
 
@@ -103,27 +156,35 @@ void display()
     glLoadIdentity();
     c.updateCamera();
 
-    angle += 0.1;
+    scale= scene_max.x-scene_min.x;
+    scale= max(scene_max.y - scene_min.y,scale);
+    scale= max(scene_max.z - scene_min.z,scale);
+    scale= 2.f / scale;
+    glScalef(scale, scale, scale);
+
+    glTranslatef( -scene_center.x, -scene_center.y, -scene_center.z );
+
+    angle += 0.9;
     glRotatef(angle, 0, 1, 0);
-	recursive_render(scene, scene->mRootNode);
+    recursive_render(scene, scene->mRootNode);
 
     glutSwapBuffers();
 }
 
 /* Timer to refresh every 15ms */
 void timer(int value) {
-   glutPostRedisplay();
-   glutTimerFunc(50, timer, 0);
+    glutPostRedisplay();
+    glutTimerFunc(50, timer, 0);
 }
 
 /* Resize the window */
 void reshape(GLsizei width, GLsizei height) {
-   if (height == 0) height = 1;
-   GLfloat aspect = (GLfloat)width / (GLfloat)height;
-   glViewport(0, 0, width, height);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+    if (height == 0) height = 1;
+    GLfloat aspect = (GLfloat)width / (GLfloat)height;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
 
 int main(int argc, char** argv)
@@ -134,15 +195,19 @@ int main(int argc, char** argv)
 
     scene = aiImportFile(argv[1],aiProcessPreset_TargetRealtime_MaxQuality);
 
-	if (!scene) {
-		return 1;
-	}
+    if (!scene) {
+        return 1;
+    }
+    get_bounding_box(&scene_min,&scene_max);
+    scene_center.x = (scene_min.x + scene_max.x) / 2.0f;
+    scene_center.y = (scene_min.y + scene_max.y) / 2.0f;
+    scene_center.z = (scene_min.z + scene_max.z) / 2.0f;
 
 
     // m.printVertex();
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(640, 480);
     glutInitWindowPosition(50, 50);
     glutCreateWindow("IN55 - Animation rendering");
@@ -151,7 +216,7 @@ int main(int argc, char** argv)
     initGL();
     glutTimerFunc(0, timer, 0);
     glutMainLoop();
-	aiReleaseImport(scene);
+    aiReleaseImport(scene);
     return 0;
 }
 
